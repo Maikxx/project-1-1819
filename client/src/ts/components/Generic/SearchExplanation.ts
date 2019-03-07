af567f9ab711f25ab0c7b83e74a337b31f315f12
Maikel van Veen <maikeloo10@hotmail.com>
2019-03-07 12:50:13 +0100

    fix(*): fix mobile styling

--- client/src/ts/components/Generic/SearchExplanation.ts
+++ client/src/ts/components/Generic/SearchExplanation.ts
@@ -2,6 +2,7 @@ import { Component } from '../../utils/Component'
 import { M } from '../../utils/Engine'
 import { Paragraph } from '../Core/DataDisplay/Text/Paragraph'
 import { Heading } from '../Core/DataDisplay/Text/Heading'
+import { WrapView } from '../Core/DataDisplay/WrapView'
 
 interface Props {
     className?: string
@@ -13,34 +14,37 @@ export class SearchExplanation extends Component<Props> {
     }
 
     public render = () => {
-        return M.create('article', { className: this.getClassNames() }, [
-            new Heading({
-                className: `SearchExplanation__title`,
-                level: 2,
-                children: ['Doorzoek de bibliotheekcatalogus van de Openbare Bibliotheek Amsterdam'],
-            }),
-            new Paragraph({
-                isBlock: true,
-                children: [
-                    `1. Selecteer een locatie op de kaart hieronder waarbinnen u graag op onderzoek uit zou willen gaan.${` `}
-                    Let op, de locaties met de rode rand kunnen niet worden geselecteerd, omdat de etagetekeningen niet zijn vrijgegeven.`,
-                ],
-            }),
-            new Paragraph({
-                isBlock: true,
-                children: [
-                    `2. Vervolgens krijgt u een representatie van het gebouw te zien per etage,${` `}
-                    met binnen iedere etage alle secties die bekend zijn.`,
-                ],
-            }),
-            new Paragraph({
-                isBlock: true,
-                children: [
-                    `3. Als u een sectie hebt gevonden waarvan u graag suggesties krijgt,${``}
-                    klikt u daarop en krijgt u de suggesties te zien die in deze bibliotheek beschikbaar zijn.`,
-                ],
-            }),
-        ])
+        return new WrapView({
+            className: this.getClassNames(),
+            children: [
+                new Heading({
+                    className: `SearchExplanation__title`,
+                    level: 2,
+                    children: ['Doorzoek de bibliotheekcatalogus van de Openbare Bibliotheek Amsterdam'],
+                }),
+                new Paragraph({
+                    isBlock: true,
+                    children: [
+                        `1. Selecteer een locatie op de kaart hieronder waarbinnen u graag op onderzoek uit zou willen gaan.${` `}
+                        Let op, de locaties met de rode rand kunnen niet worden geselecteerd, omdat de etagetekeningen niet zijn vrijgegeven.`,
+                    ],
+                }),
+                new Paragraph({
+                    isBlock: true,
+                    children: [
+                        `2. Vervolgens krijgt u een representatie van het gebouw te zien per etage,${` `}
+                        met binnen iedere etage alle secties die bekend zijn.`,
+                    ],
+                }),
+                new Paragraph({
+                    isBlock: true,
+                    children: [
+                        `3. Als u een sectie hebt gevonden waarvan u graag suggesties krijgt,${``}
+                        klikt u daarop en krijgt u de suggesties te zien die in deze bibliotheek beschikbaar zijn.`,
+                    ],
+                }),
+            ],
+        }).render()
     }
 
     private getClassNames() {
