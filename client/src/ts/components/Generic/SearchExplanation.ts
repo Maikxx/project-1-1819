5e630fec73b5e2e2cc721bb8c721b4ad7d910551
Maikel van Veen <maikeloo10@hotmail.com>
2019-03-08 11:01:50 +0100

    feat(searchExplanation): enhance search explanation

--- client/src/ts/components/Generic/SearchExplanation.ts
+++ client/src/ts/components/Generic/SearchExplanation.ts
@@ -25,22 +25,25 @@ export class SearchExplanation extends Component<Props> {
                 new Paragraph({
                     isBlock: true,
                     children: [
-                        `1. Selecteer een locatie op de kaart hieronder waarbinnen u graag op onderzoek uit zou willen gaan.${` `}
-                        Let op, de locaties met de rode rand kunnen niet worden geselecteerd, omdat de etagetekeningen niet zijn vrijgegeven.`,
+                        `1. Selecteer een van de beschikbare locaties op de kaart, waar u graag de collectie zou willen ontdekken.`,
                     ],
                 }),
                 new Paragraph({
                     isBlock: true,
                     children: [
-                        `2. Vervolgens krijgt u een representatie van het gebouw te zien per etage,${` `}
-                        met binnen iedere etage alle secties die bekend zijn.`,
+                        `2. Vervolgens krijgt u een representatie van het gebouw te zien per etage, waar in iedere etage alle secties die bekend zijn, zijn uitgelicht.`,
                     ],
                 }),
                 new Paragraph({
                     isBlock: true,
                     children: [
-                        `3. Als u een sectie hebt gevonden waarvan u graag suggesties krijgt,${``}
-                        klikt u daarop en krijgt u de suggesties te zien die in deze bibliotheek beschikbaar zijn.`,
+                        `3. Als u een sectie hebt gevonden waarvan u graag suggesties krijgt, klikt u daarop.`,
+                    ],
+                }),
+                new Paragraph({
+                    isBlock: true,
+                    children: [
+                        `4. U krijgt nu suggesties te zien op basis van uw selectie. Hier kunt u doorklikken op een suggestie voor meer informatie.`,
                     ],
                 }),
             ],
